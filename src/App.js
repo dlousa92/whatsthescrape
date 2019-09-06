a2ce7aaaaad8c2bd6de4243e91750fa640514065
Daniel Lousa <dlousa92@gmail.com>
2019-09-06 12:42:55 -0400

    add state to component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,36 @@ import './App.css'
 import Form from './components/Form/Form'
 
 class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      url: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange (e) {
+    e.preventDefault(
+      console.log(e)
+    )
+  }
+
+  handleSubmit (e) {
+    e.preventDefault()
+    console.log(e)
+  }
+
   render () {
     return (
       <div className='App'>
         <header className='App-header'>
-          <Form hello='hello' />
+          <Form
+            hello='hello'
+            handleChange={this.handleChange}
+            handleSubmit={this.handleSubmit}
+          />
         </header>
       </div>
     )
