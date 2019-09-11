8d1e8b2d529a23e793aae27ffd68f80b07ff8701
Daniel Lousa <dlousa92@gmail.com>
2019-09-11 15:37:39 -0400

    change color of carousel icons

--- src/components/ImageCarousel/ImageCarousel.js
+++ src/components/ImageCarousel/ImageCarousel.js
@@ -21,6 +21,14 @@ const ImageCarousel = (props) => {
             </Carousel.Item>
           )
         })}
+        <a class='carousel-control-prev' href='#carouselExampleIndicators' role='button' data-slide='prev'>
+          <span class='carousel-control-prev-icon' aria-hidden='true' />
+          <span class='sr-only'>Previous</span>
+        </a>
+        <a class='carousel-control-next' href='#carouselExampleIndicators' role='button' data-slide='next'>
+          <span class='carousel-control-next-icon' aria-hidden='true' />
+          <span class='sr-only'>Next</span>
+        </a>
       </Carousel>
       <p className='carousel-text'>There are a total of {images.length} viewable images on {url}</p>
     </div>
