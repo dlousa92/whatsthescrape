c35c30c98201de55603ec11ec83a24c19a83b60c
Daniel Lousa <dlousa92@gmail.com>
2020-08-27 15:16:07 -0400

    prepare app for deployment

--- src/App.js
+++ src/App.js
@@ -1,145 +1,154 @@
-import React from 'react'
-import Header from './components/Header/Header'
-import Form from './components/Form/Form'
-import ImageCarousel from './components/ImageCarousel/ImageCarousel'
-import Chart from './components/Chart/Chart'
-import './App.scss'
-import axios from 'axios'
+import React from "react";
+import Header from "./components/Header/Header";
+import Form from "./components/Form/Form";
+import ImageCarousel from "./components/ImageCarousel/ImageCarousel";
+import Chart from "./components/Chart/Chart";
+import "./App.scss";
+import axios from "axios";
 
 class App extends React.Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      url: '',
+      url: "",
       wordTotal: 0,
       topTenWords: [],
-      imagesOnSite: []
-    }
+      imagesOnSite: [],
+    };
 
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
-    this.getTopTenWords = this.getTopTenWords.bind(this)
-    this.setImagesOnSite = this.setImagesOnSite.bind(this)
-    this.displayResults = this.displayResults.bind(this)
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.getTopTenWords = this.getTopTenWords.bind(this);
+    this.setImagesOnSite = this.setImagesOnSite.bind(this);
+    this.displayResults = this.displayResults.bind(this);
   }
 
-  getTopTenWords (textArray) {
-    let wordCounts = {}
-    let topTen = []
+  getTopTenWords(textArray) {
+    let wordCounts = {};
+    let topTen = [];
 
     // Don't add words that are too short and most conjuctions, i.e a, in, for, the
-    textArray.forEach(word => {
+    textArray.forEach((word) => {
       if (word.length <= 3) {
-        return
+        return;
       }
 
       if (wordCounts[word]) {
         // If this word is already in our object, increase the count of that word by 1
-        wordCounts[word]++
+        wordCounts[word]++;
       } else {
         // Otherwise, say that we've found one of that word so far.
-        wordCounts[word] = 1
+        wordCounts[word] = 1;
       }
-    })
+    });
     // Create an array of each word and its count
-    Object.keys(wordCounts).forEach(word => {
+    Object.keys(wordCounts).forEach((word) => {
       topTen.push({
         name: `${word}`,
-        weight: wordCounts[word]
-      })
-    })
+        weight: wordCounts[word],
+      });
+    });
     // Sort the words by descending order
     topTen.sort(function (a, b) {
-      return b.weight - a.weight
-    })
+      return b.weight - a.weight;
+    });
     // Cut down array to the top ten words
     this.setState({
-      topTenWords: topTen.slice(0, 10)
-    })
+      topTenWords: topTen.slice(0, 10),
+    });
   }
 
-  setImagesOnSite (images) {
-    let imagesOnSiteWithDuplicates = []
-    let imagesOnSite = []
+  setImagesOnSite(images) {
+    let imagesOnSiteWithDuplicates = [];
+    let imagesOnSite = [];
 
     // If image is stored locally, do not add it as a viewable image to our images on site array, otherwise push it into our images with duplicates array
-    images.forEach(image => {
-      if (image.src.substr(0, 4) !== 'http') {
-        return
+    images.forEach((image) => {
+      if (image.src.substr(0, 4) !== "http") {
+        return;
       }
-      imagesOnSiteWithDuplicates.push(image)
-    })
+      imagesOnSiteWithDuplicates.push(image);
+    });
 
     // Removes duplicate images from our images with duplicates array
-    function removeDuplicates (imagesOnSiteWithDuplicates, src) {
-      let newArray = []
-      let lookupObject = {}
+    function removeDuplicates(imagesOnSiteWithDuplicates, src) {
+      let newArray = [];
+      let lookupObject = {};
 
       for (var i in imagesOnSiteWithDuplicates) {
-        lookupObject[imagesOnSiteWithDuplicates[i][src]] = imagesOnSiteWithDuplicates[i]
+        lookupObject[imagesOnSiteWithDuplicates[i][src]] =
+          imagesOnSiteWithDuplicates[i];
       }
 
       for (i in lookupObject) {
-        newArray.push(lookupObject[i])
+        newArray.push(lookupObject[i]);
       }
-      return newArray
+      return newArray;
     }
 
-    imagesOnSite = removeDuplicates(imagesOnSiteWithDuplicates, 'src')
+    imagesOnSite = removeDuplicates(imagesOnSiteWithDuplicates, "src");
 
     this.setState({
-      imagesOnSite: imagesOnSite
-    })
+      imagesOnSite: imagesOnSite,
+    });
   }
 
-  handleChange (e) {
+  handleChange(e) {
     this.setState({
-      url: e.target.value
-    })
+      url: e.target.value,
+    });
   }
 
-  handleSubmit (e) {
-    e.preventDefault()
+  handleSubmit(e) {
+    e.preventDefault();
 
     // First axios request gets text, second one gets images
-    axios.all([
-      axios.get('http://localhost:8000/scrape', {
-        params: {url: this.state.url}
-      }),
-      axios.get('http://localhost:8000/scrape-img', {
-        params: {url: this.state.url}
-      })
-    ]).then(axios.spread((textRes, imageRes) => {
-      const text = textRes.data
-      const textArray = text.split(' ')
-      const images = imageRes.data
+    axios
+      .all([
+        axios.get("https://whatsthescrape.herokuapp.com/scrape", {
+          params: { url: this.state.url },
+        }),
+        axios.get("https://whatsthescrape.herokuapp.com/scrape-img", {
+          params: { url: this.state.url },
+        }),
+      ])
+      .then(
+        axios.spread((textRes, imageRes) => {
+          const text = textRes.data;
+          const textArray = text.split(" ");
+          const images = imageRes.data;
 
-      // Add up total amount of words
-      this.setState({
-        wordTotal: textArray.length
-      })
+          // Add up total amount of words
+          this.setState({
+            wordTotal: textArray.length,
+          });
 
-      // Run our data formatting functions
-      this.setImagesOnSite(images)
-      this.getTopTenWords(textArray)
-    }))
+          // Run our data formatting functions
+          this.setImagesOnSite(images);
+          this.getTopTenWords(textArray);
+        })
+      );
   }
 
-  displayResults () {
+  displayResults() {
     if (this.state.topTenWords.length === 0) {
-      return
+      return;
     }
     return (
       <div>
         <ImageCarousel url={this.state.url} images={this.state.imagesOnSite} />
-        <Chart url={this.state.url} topTenWords={this.state.topTenWords} wordTotal={this.state.wordTotal} />
+        <Chart
+          url={this.state.url}
+          topTenWords={this.state.topTenWords}
+          wordTotal={this.state.wordTotal}
+        />
       </div>
-    )
+    );
   }
 
-  render () {
+  render() {
     return (
-      <div className='app'>
+      <div className="app">
         <Header />
 
         <Form
@@ -149,8 +158,8 @@ class App extends React.Component {
         />
         {this.displayResults()}
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
